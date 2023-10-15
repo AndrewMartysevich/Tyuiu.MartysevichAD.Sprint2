14b830d6280e67a553563d77b39e9cbb12430ed2
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-10-15 17:32:43 +0500

    Task4

---
+++ Tyuiu.MartysevichAD.Sprint2.Task4.V11.Test/DataServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.MartysevichAD.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint2.Task4.V11.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCondition1()
+        {
+            DataService ds = new DataService();
+            double x = 3;
+            double y = 4;
+            double res = ds.Calculate(x, y);
+            double wait = 228.719;
+            Assert.AreEqual(wait, res);
+        }
+        public void ValidCondition2()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = -160;
+            double res = ds.Calculate(x, y);
+            double wait = 0.013;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MartysevichAD.Sprint2.Task4.V11/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.MartysevichAD.Sprint2.Task4.V11.Lib;
+
+namespace Tyuiu.MartysevichAD.Sprint2.Task4.V11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+            Console.Title = "Спринт #2 | Выполнил: Мартысевич А. Д. | ИИПб-23-3";
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* Спринт #2                                                              *");
+            Console.WriteLine("* Тема: тернарный оператор                                               *");
+            Console.WriteLine("* Задание #4                                                             *");
+            Console.WriteLine("* Вариант #11                                                            *");
+            Console.WriteLine("* Выполнил: Мартысевич Андрей Денисович | ИИПб-23-3                      *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                               *");
+            Console.WriteLine("* Написать программу, которая вычисляет требуемое значение               *");
+            Console.WriteLine("* с использованием тернарного оператора,                                 *");
+            Console.WriteLine("* где пользователь вводит значение переменных x, y с клавиатуры.         *");
+            Console.WriteLine("*                                                                        *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                       *");
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("Введите значение переменной Х");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной Y");
+            double y = Convert.ToDouble(Console.ReadLine());
+            double res = ds.Calculate(x, y);
+
+            Console.WriteLine("**************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("**************************************************************************");
+
+            Console.WriteLine("Значение функции " + res);
+            Console.ReadKey();
+        }
+    }
+}
