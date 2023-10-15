9f8f15f605244358a8dc0fc2078edb4cf45638e3
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-10-15 06:24:09 +0500

    Add project files.

---
+++ Tyuiu.MartysevichAD.Sprint2.Task0.V27.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MartysevichAD.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MartysevichAD.Sprint2.Task0.V27.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6d67b37d-c52b-4ea3-9ca1-2e7b689d6593")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
