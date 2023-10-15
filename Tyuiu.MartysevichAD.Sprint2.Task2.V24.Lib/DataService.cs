f26e17d843c6528235139d41668f2e3916703e89
Tyuiu.MartysevichAD <Tyuiu.MartysevichAD@outlook.com>
2023-10-15 11:38:05 +0500

    Task2

---
+++ Tyuiu.MartysevichAD.Sprint2.Task2.V24.Lib/DataService.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.MartysevichAD.Sprint2.Task2.V24.Lib
+{
+    public class DataService : ISprint2Task2V24
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool res;
+            if ((x >= 3) && (x <= 13) && (y >= 3) && (y <= 12))
+            {
+                res = true;
+            }
+            else
+            {
+                res = false;
+            }           
+            if ((x >= 3) && (x <= 5) && (y >= 8) && (y <= 10))
+            {
+                res = false;
+            }
+            if ((x >= 6) && (x <= 8) && (y >= 3) && (y <= 4))
+            {
+                res = false;
+            }
+            if ((x >= 9) && (x <= 12) && (y >= 7) && (y <= 8))
+            {
+                res = false;
+            }
+            if ((x >= 8) && (x <= 10) && (y >= 11) && (y <= 12))
+            {
+                res = false;
+            }
+            if ((x >= 3) && (x <= 6) && (y == 12))
+            {
+                res = false;
+            }
+            if ((x == 13)&& (y != 6))
+            {
+                res = false;
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.MartysevichAD.Sprint2.Task2.V24.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.MartysevichAD.Sprint2.Task2.V24.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.MartysevichAD.Sprint2.Task2.V24.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4f7b7e66-63e5-41e2-aeac-b72262fef44f")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
